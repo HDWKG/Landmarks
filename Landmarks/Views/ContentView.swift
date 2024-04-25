aa12593734979acc2abcae93b0cd8927f557f10d
Nathan <ndsutejo@gmail.com>
2024-04-25 11:27:19 +0700

    p

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by student on 19/04/24.
+//
+
+import SwiftUI
+import MapKit
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+#Preview {
+    ContentView()
+        .environment(ModelData())
+}
---
+++ Landmarks/Views/PageView/PageControl.swift
@@ -0,0 +1,8 @@
+//
+//  PageControl.swift
+//  Landmarks
+//
+//  Created by student on 25/04/24.
+//
+
+import Foundation
